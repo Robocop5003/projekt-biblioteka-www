b831345b7a62523b0d8d278770cf5f3b42cd2555
TERRsalt <tomekgimik05@gmail.com>
2026-03-21 14:59:16 +0100

    new and fix: Jasny i ciemny motyw, customowe kolorki, zmiana czcionki, i zmiany w sticky  + zmiany w komentarzach i dodatkowe entery dla lepszej czytelności

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ function menuClick(){
     console.log("Menu kliknięte");
 }
 
-function modeSwitchClick(){
-    console.log("Zmiana motywu kliknięta");
+function modeSwitchClick() {
+    if (document.body.classList.contains("light")) {
+        document.body.classList.remove("light");
+        document.body.classList.add("dark");
+        localStorage.setItem("theme", "dark");
+    } 
+    
+    else {
+        document.body.classList.remove("dark");
+        document.body.classList.add("light");
+        localStorage.setItem("theme", "light");
+    } 
 }
